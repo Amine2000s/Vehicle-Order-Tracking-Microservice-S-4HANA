f6f73f757c9376c79d47b01944b17e5e3d842ad7
Chabi Amine sif eddine <chabiaminesifeddine@gmail.com>
2025-08-09 06:21:02 +0100

    add initial files

---
+++ src/main/java/com/chabiaminesifeddine/vehiculeTrackingMicroservice/VehiculeTrackingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.chabiaminesifeddine.vehiculeTrackingMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculeTrackingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculeTrackingMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chabiaminesifeddine/vehiculeTrackingMicroservice/VehiculeTrackingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chabiaminesifeddine.vehiculeTrackingMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculeTrackingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
